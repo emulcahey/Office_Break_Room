37d14263c1dc34966b1c64686469689205c30deb
juliette maloke <your@email.com>
2024-06-10 21:03:21 -0500

    Handles routes for user registration

---
+++ routes/authRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { register, login, logout } = require('../controllers/authController');
+
+const router = express.Router();
+
+// Register route
+router.post('/register', register);
+
+// Login route
+router.post('/login', login);
+
+// Logout route
+router.get('/logout', logout);
+
+module.exports = router;

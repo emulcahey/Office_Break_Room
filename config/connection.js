56874a9f872321ebb64201be5b251e021ee6e6fc
Austin L <161216998+Kushonim@users.noreply.github.com>
2024-06-17 17:52:15 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = 
-  new Sequelize(
+const sequelize = process.env.DB_URL
+  ? new Sequelize (process.env.DB_URL)
+  : new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT || 5432,
       dialect: process.env.DB_DIALECT || 'postgres'
     }
   );
